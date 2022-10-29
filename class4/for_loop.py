ac65f1407896b3570cdfa262fa9e806270b24327
somename <a@gmail.com>
2022-10-29 02:32:09 +0000

    added class4

---
+++ class4/for_loop.py
@@ -0,0 +1,15 @@
+# for items in range(10):
+       # print(items+1)
+USERS=["tom","ken","lisa"]
+# for user_list in USERS:
+#         print(user_list)
+# for user_list in USERS:
+#         if user_list == "ken":
+#             print(user_list)
+
+file1 = open("/etc/passwd")
+# for lines in file1:
+#     # print(lines)
+#     print(lines.strip())
+file1.close()
+
